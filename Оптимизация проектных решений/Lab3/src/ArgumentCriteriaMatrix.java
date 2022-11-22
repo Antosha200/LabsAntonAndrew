53ab73fadffa45f5c56c917c791c9182805f6aee
mo2x <borodovsky1243@gmail.com>
2022-11-22 09:23:40 +0300

    нормализация

--- Оптимизация проектных решений/Lab3/src/ArgumentCriteriaMatrix.java
+++ Оптимизация проектных решений/Lab3/src/ArgumentCriteriaMatrix.java
@@ -3,17 +3,19 @@ import java.util.Scanner;
 public class ArgumentCriteriaMatrix {
     private int[][] data;
     private int[][] rotateData;
-    private int argumentCunt;
+    private double[][] normalData;
+    private int argumentCount;
     private boolean[] compromises;
     private boolean[] criteriaMaxMin = Lab.CRITERIA_MAX_MIN;
 
     public ArgumentCriteriaMatrix(Scanner scanner)
     {
-        argumentCunt = scanner.nextInt();
-        compromises = new boolean[argumentCunt];
-        data = new int[argumentCunt][Lab.CRITERIA_COUNT];
-        rotateData = new int[argumentCunt][Lab.CRITERIA_COUNT];
-        for (int i = 0; i < argumentCunt; i++)
+        argumentCount = scanner.nextInt();
+        compromises = new boolean[argumentCount];
+        data = new int[argumentCount][Lab.CRITERIA_COUNT];
+        rotateData = new int[argumentCount][Lab.CRITERIA_COUNT];
+        normalData = new double[argumentCount][Lab.CRITERIA_COUNT];
+        for (int i = 0; i < argumentCount; i++)
         {
             for (int j = 0; j < Lab.CRITERIA_COUNT; j++)
             {
@@ -24,6 +26,7 @@ public class ArgumentCriteriaMatrix {
         getMainValues();
         arrPareto();
         rotateUP();
+        normalization();
     }
 
     private void getMainValues()
@@ -32,7 +35,7 @@ public class ArgumentCriteriaMatrix {
         {
             int value = data[0][j];
             int argWithValue = 0;
-            for (int i = 1; i < argumentCunt; i++)
+            for (int i = 1; i < argumentCount; i++)
             {
                 if ((criteriaMaxMin[j] && value < data[i][j])||(!criteriaMaxMin[j] && value > data[i][j]))
                 {
@@ -46,11 +49,11 @@ public class ArgumentCriteriaMatrix {
 
     private void arrPareto()
     {
-        for (int i = 0; i < argumentCunt; i++)
+        for (int i = 0; i < argumentCount; i++)
         {
             if (!compromises[i])
             {
-                for (int j = 0; j < argumentCunt; j++)
+                for (int j = 0; j < argumentCount; j++)
                 {
                     if (i != j)
                     {
@@ -81,7 +84,7 @@ public class ArgumentCriteriaMatrix {
     {
         for (int j = 0; j < Lab.CRITERIA_COUNT; j++)
         {
-            for (int i = 0; i < argumentCunt; i++)
+            for (int i = 0; i < argumentCount; i++)
             {
                 if (!criteriaMaxMin[j])
                 {
@@ -94,27 +97,72 @@ public class ArgumentCriteriaMatrix {
             }
         }
     }
+
+    private void normalization()
+    {
+        for (int j = 0; j < Lab.CRITERIA_COUNT; j++)
+        {
+            int max = rotateData[0][j];
+            int min = rotateData[0][j];
+            for (int i = 1; i < argumentCount; i++)
+            {
+                if (compromises[i])
+                {
+                    if (max < rotateData[i][j])
+                    {
+                        max = rotateData[i][j];
+                    }
+                    if (min > rotateData[i][j])
+                    {
+                        min = rotateData[i][j];
+                    }
+                }
+            }
+            for (int i = 0; i < argumentCount; i++)
+            {
+                if (compromises[i])
+                {
+                    normalData[i][j] = (rotateData[i][j] - min) / (double)(max - min);
+                }
+            }
+        }
+    }
+
     public void printMatrix()
     {
-        for (int i = 0; i < argumentCunt; i++)
+        for (int i = 0; i < argumentCount; i++)
         {
             for (int j = 0; j < Lab.CRITERIA_COUNT; j++)
             {
                 System.out.printf("%5d",data[i][j]);
             }
+            System.out.print(" |");
             if (compromises[i])
             {
-                System.out.printf("%5s","K");
+                System.out.printf("%2s","K");
             }
             else
             {
-                System.out.printf("%5s","C");
+                System.out.printf("%2s","C");
             }
+            System.out.print(" |");
             for (int j = 0; j < Lab.CRITERIA_COUNT; j++)
             {
                 if (compromises[i])
                 {
                     System.out.printf("%5d", rotateData[i][j]);
+                }
+                else
+                {
+                    System.out.print("     ");
+                }
+            }
+            System.out.print(" |");
+            for (int j = 0; j < Lab.CRITERIA_COUNT; j++)
+            {
+                if (compromises[i])
+                {
+                    System.out.printf("%6.2f", normalData[i][j]);
                 }
                 else
                 {
